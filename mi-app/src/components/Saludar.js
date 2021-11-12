88ba348acec78f9041540e94a0e7eb6a46b0e065
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-11-11 22:00:04 -0600

    funcion Saludar con const nombre, userinfo y saludarFn

--- mi-app/src/components/Saludar.js
+++ mi-app/src/components/Saludar.js
@@ -2,13 +2,14 @@ import React from "react";
 
 
 export default function Saludar(props) {
-  const saludar=() =>{
+ const {userInfo, saludarFn}= props;
+ const {nombre} = userInfo;
     console.log (props);
-  };
+    console.log(userInfo);
     
     return(
    <div>     
-  <button onClick={props.saludarFn(props.userInfo.nombre)}>Saludar</button>
+  <button onClick={() => saludarFn(nombre)}>Saludar</button>
   </div>
     );
 
